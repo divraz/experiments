bde237e3412c94e9015046cb88f3cf8ae794f1e8
divyanshuraj6815 <divyanshuraj.6815@gmail.com>
2023-01-03 19:22:59 +0000

    draw_circle

---
+++ ros_experiment_1_ws/build/my_robot_controller/share/my_robot_controller/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/experiments/ros_experiment_1_ws/build/my_robot_controller"
--- ros_experiment_1_ws/src/my_robot_controller/setup.py
+++ ros_experiment_1_ws/src/my_robot_controller/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "draw_circle = my_robot_controller.draw_circle:main"
         ],
     },
 )
